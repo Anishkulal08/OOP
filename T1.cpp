d6f4a82d70c53d43051e9efc888e5c4822c90faf
Anish <anishmoolya05@gmail.com>
2026-08-06 20:43:59 +0530

    Added first program

---
+++ T1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+	int a,b;
+	std::cout<<"Enter value for A and B\n";
+	std::cin>>a>>b;
+	std::cout<<"Before exchanging";
+	std::cout<<"\nA value is:"<<a<<"\nB value is:"<<b;
+	a=b+a;
+	b=a-b;
+	a=a-b;
+	std::cout<<"\nAfter exchanging";
+	std::cout<<"\nA value is:"<<a<<"\nB value is:"<<b;
+	return 0;
+}
